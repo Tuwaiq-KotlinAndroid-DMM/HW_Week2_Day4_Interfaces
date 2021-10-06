517f0d6136ab4d69c3fe72609a61133d43ec7d33
ibtihala817 <Ibtihal.ibrahim.dmm.k1@tuwaiq.edu.sa>
2021-10-07 01:26:41 +0300

    hw

--- HWDAY4Week2/src/main/kotlin/Main.kt
+++ HWDAY4Week2/src/main/kotlin/Main.kt
@@ -1,20 +1,3 @@
 import com.kotlin.vehicle.SportCar
 import com.kotlin.vehicle.Truck
 
-fun main() {
-    val sportCar = SportCar(true,"SportCar",80,5)
-    println(sportCar.isMoving())
-    println(sportCar.gear)
-    println(sportCar.speed)
-    println(sportCar.printStates())
-
-
-
-    val truck = Truck(false,"Truck",40,2)
-    println(truck.isMoving())
-    println(truck.gear)
-    println(truck.speed)
-    println(truck.printStates())
-
-
-}
---
+++ HWDAY4Week2/src/main/kotlin/com/kotlin/vehicle/main.kt
@@ -0,0 +1,19 @@
+package com.kotlin.vehicle
+
+fun main() {
+    val sportCar = SportCar(true,"SportCar",80,5)
+    println(sportCar.isMoving())
+    println(sportCar.gear)
+    println(sportCar.speed)
+    println(sportCar.printStates())
+
+
+
+    val truck = Truck(false,"Truck",40,2)
+    println(truck.isMoving())
+    println(truck.gear)
+    println(truck.speed)
+    println(truck.printStates())
+
+
+}
